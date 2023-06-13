7bb6c34a8920eed42717673b906ab39f3923165b
oat431 <oat431@gmail.com>
2023-06-14 03:29:14 +0700

    🛠 setup: websocket fe

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="lg:mx-60 lg:my-6 lg:px-3 sm:mx-10 sm:my-2 sm:px-1 border p-3">
+    <div class="overflow-x-auto">
+      <HelloWorld msg="Vite + Vue" />
+    </div>
+  </div>
+</template>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+defineProps<{ msg: string }>();
+const msg = ref("placeholder");
+
+const count = ref(0);
+</script>
+
+<template>
+  <div class="card mt-3">
+    <button class="btn btn-primary" type="button" @click="count++">
+      count is {{ count }}
+    </button>
+    <div class="mockup-code mt-3">
+      <pre data-prefix=">" class="text-success">
+        <code>Edit components/HelloWorld.vue</code> to test HMR
+      </pre>
+      <pre data-prefix=">" class="text-warning">
+        <code>Check out <a class="link link-warning" href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>, the official Vue + Vite starter</code>
+      </pre>
+      <pre data-prefix=">" class="text-secondary">
+        <code>Intall <a class="link link-secondary" href="https://github.com/vuejs/language-tools" target="_blank">Volar</a> in your IDE for a better DX</code>
+      </pre>
+      <pre data-prefix=">" class="text-error">
+        <code>{{ msg }}</code>
+      </pre>
+    </div>
+  </div>
+</template>
